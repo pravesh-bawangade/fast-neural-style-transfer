194db9f0a7c045fa2e5a153022dc37b788f181a2
Pravesh Bawangade <pptechno52@gmail.com>
2020-06-09 14:59:33 +0530

    added utils.py for overlayimage and updated realTime.py

--- realTime.py
+++ realTime.py
@@ -8,11 +8,12 @@ import torch
 from torch.autograd import Variable
 import os
 import cv2
+from utils import overlay_emoji as oe
 
 
-def main(checkpoint_models):
+def main(checkpoint_models, images_list):
 
-    cap = cv2.VideoCapture("video/input.mp4")
+    cap = cv2.VideoCapture(0)
 
     os.makedirs("images/outputs", exist_ok=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,17 +22,21 @@ def main(checkpoint_models):
     # Define model and load model checkpoint
     transformer = TransformerNet().to(device)
 
-    for checkpoint_model in checkpoint_models:
-        transformer.load_state_dict(torch.load(checkpoint_model, map_location=torch.device('cpu')))
+    # For recording video
+    frame_width = int(760)
+    frame_height = int(240)
+    out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 4, (frame_width, frame_height))
+
+    for i in range(len(checkpoint_models)):
+        transformer.load_state_dict(torch.load(checkpoint_models[i], map_location=torch.device('cpu')))
         transformer.eval()
 
-        # For recording video
-        # frame_width = int(760)
-        # frame_height = int(240)
-        # out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 15, (frame_width, frame_height))
         while True:
             ret, frame = cap.read()
-            frame = cv2.resize(frame, (320, 240))
+            frame = cv2.resize(frame, (380, 240))
+            overlay_painting = cv2.imread(images_list[i])
+            overlay_painting = cv2.resize(overlay_painting,(130,90))
+            background = oe(frame.copy(), overlay_painting, 250, 150)
             if ret:
                 # Prepare input frame
                 image_tensor = Variable(transform(frame)).to(device).unsqueeze(0)
@@ -41,12 +46,20 @@ def main(checkpoint_models):
                 # Add to frames
                 output_img = cv2.cvtColor(deprocess(stylized_image), cv2.COLOR_BGR2RGB)
 
-                cv2.imshow("image", output_img)
-                # out.write(output_img)
+                background = (background - np.min(background)) / (
+                        np.max(background) - np.min(background)
+                )  # this set the range from 0 till 1
+                background = (background * 255).astype(np.uint8)
+                vis = np.concatenate((background, output_img), axis=1)
+
+                frame_normed = np.array(vis, np.uint8)
+
+                cv2.imshow("image", frame_normed)
+                out.write(frame_normed)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
-    # out.release()
+    out.release()
     cap.release()
     cv2.destroyAllWindows()
 
@@ -55,4 +68,7 @@ if __name__ == "__main__":
     checkpoint_model = ["checkpoints/mosaic_8000.pth", "checkpoints/shakuntala_4000.pth",
                         "checkpoints/starry_night_4000.pth", "checkpoints/hokage_4000.pth",
                         "checkpoints/ganesh_2000.pth"]
-    main(checkpoint_model)
+    images_list = ["src/images/styles/mosaic.jpg", "src/images/styles/shakuntala.jpg",
+                   "src/images/styles/starry_night.jpg", "src/images/styles/hokage.jpg",
+                   "src/images/styles/ganesh.jpg"]
+    main(checkpoint_model, images_list)
